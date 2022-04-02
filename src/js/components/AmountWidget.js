99e5626e384f16c15cb9f6025a9ea5aa04dd959c
Leszek Wydro <leszek.wydro@gmail.com>
2022-04-02 14:15:21 +0200

    renderr value fix

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -3,13 +3,15 @@ import BaseWidget from './BaseWidget.js';
 
 class AmountWidget extends BaseWidget{
   constructor(element){
-    super(element, settings.amountWidget.defaultValue );
+    super(element, settings.amountWidget.defaultValue);
 
     const thisWidget = this;
 
     thisWidget.getElements(element);
     
     thisWidget.initAction();
+
+    thisWidget.renderValue();
 
     // console.log('AmountWidget:', thisWidget);
     // console.log('constructor arguments:', element);
@@ -26,7 +28,7 @@ class AmountWidget extends BaseWidget{
 
   isValid(value){
     return !isNaN(value) 
-    && value >= settings.amountWidget.defaultMin  
+    && value >= settings.amountWidget.defaultMin   
     && value <= settings.amountWidget.defaultMax;
   }
 
@@ -41,7 +43,7 @@ class AmountWidget extends BaseWidget{
       
     thisWidget.dom.input.addEventListener('change', function(){
       // thisWidget.setValue(thisWidget.dom.input.value);
-      thisWidget.setValue = thisWidget.dom.input.value;
+      thisWidget.value = thisWidget.dom.input.value;
     });
 
     thisWidget.dom.linkDecrease.addEventListener('click', function(event){
--- src/js/components/BaseWidget.js
+++ src/js/components/BaseWidget.js
@@ -1,60 +1,54 @@
-class BaseWidget {
+class BaseWidget{
   constructor(wrapperElement, initialValue){
     const thisWidget = this;
-
     thisWidget.dom = {};
     thisWidget.dom.wrapper = wrapperElement;
-
+  
     thisWidget.correctValue = initialValue;
   }
-
-  get value(){  
+  
+  get value(){
     const thisWidget = this;
-
+  
     return thisWidget.correctValue;
   }
-
+  
   set value(value){
     const thisWidget = this;
-
     const newValue = thisWidget.parseValue(value);
-    /* TODO: Add validation */
-    if(newValue !== thisWidget.correctValue && thisWidget.isValid(newValue)){
+  
+    if(newValue !== thisWidget.correctValue && thisWidget.isValid(newValue)) {
       thisWidget.correctValue = newValue;
-      thisWidget.announce();
+      thisWidget.announce(); 
     }
-    
     thisWidget.renderValue();
   }
-
+  
   setValue(value){
     const thisWidget = this;
-
     thisWidget.value = value;
   }
-
+  
   parseValue(value){
     return parseInt(value);
   }
-
+  
   isValid(value){
     return !isNaN(value);
   }
-
-  renderValue (){
+  
+  renderValue(){
     const thisWidget = this;
-
     thisWidget.dom.wrapper.innerHTML = thisWidget.value;
   }
-
+  
   announce(){
     const thisWidget = this;
-
-    const event = new Event('updated', {
-      bubbles: true 
+    const event = new CustomEvent('updated', {
+      bubbles: true
     });
     thisWidget.dom.wrapper.dispatchEvent(event);
   }
 }
-
+  
 export default BaseWidget;
